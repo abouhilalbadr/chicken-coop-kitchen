8d82aedb7fe0c26e7fe255ca787fc122d17223b0
abouhilalbadr <abouhilal.badr@gmail.com>
2025-01-07 21:13:41 +0100

    fix build and text size

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -24,8 +24,8 @@ const setType = (type) => {
 <template>
   <div class="flex flex-col gap-6">
     <div class="flex gap-2 items-center">
-      <h2 class="text-4xl font-bold py-2">La commande N&#186 {{ item.number }}</h2>
-      <span class="text-black/60 text-xl">({{ setType(item.type) }})</span>
+      <h2 class="text-2xl font-bold py-2">La commande N&#186 {{ item.number }}</h2>
+      <span class="text-black/60 text-lg">({{ setType(item.type) }})</span>
     </div>
     <div class="flex flex-col gap-8 py-4">
       <div
@@ -35,8 +35,8 @@ const setType = (type) => {
       >
         <img :src="product.image" :alt="product.name" class="h-32 w-40 object-cover rounded-md">
         <div class="flex flex-col gap-2">
-          <h2 class="text-black font-bold text-2xl font-bree-serif">{{ product.name }} <span v-if="product.size" class="uppercase text-main">({{ product.size }})</span></h2>
-          <p class="flex flex-col gap-1 text-black/50">
+          <h2 class="text-black text-lg font-bree-serif">{{ product.name }} <span v-if="product.size" class="uppercase text-main">({{ product.size }})</span></h2>
+          <p class="flex flex-col gap-1 text-black/50 text-sm">
             <span v-if="product.viandes && product.viandes.length > 0">Les viandes: {{ product.viandes.join(',  ') }}</span>
             <span v-if="product.sauces && product.sauces.length > 0">Les sauces: {{ product.sauces.join(', ') }}</span>
             <span v-if="product.extras && product.extras.length > 0">Les extras: {{ product.extras.join(', ') }}</span>
--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -22,10 +22,10 @@ const setType = (type) => {
 </script>
 
 <template>
-  <div class="bg-white rounded-md p-4 flex flex-col gap-1 cursor-pointer">
+  <div class="bg-white rounded-md p-2 flex flex-col gap-1 cursor-pointer">
     <div class="flex gap-2 items-center">
-      <h3 class="text-2xl">La commande N&#186 {{ item.number }}</h3>
-      <span class="text-black/60 text-xl">({{ setType(item.type) }})</span>
+      <h3 class="text-lg">La commande N&#186 {{ item.number }}</h3>
+      <span class="text-black/60 text-base">({{ setType(item.type) }})</span>
     </div>
     <div v-if="showDetails" class="flex flex-col gap-4 py-4">
       <div
@@ -33,8 +33,8 @@ const setType = (type) => {
         :key="index"
         class="flex flex-col gap-2"
       >
-        <h2 class="text-black/80 text-lg font-bree-serif">{{ product.name }} <span v-if="product.size" class="uppercase text-main">({{ product.size }})</span></h2>
-        <p class="flex flex-col gap-1 text-black/40">
+        <h2 class="text-black/80 text-sm font-bree-serif">{{ product.name }} <span v-if="product.size" class="uppercase text-main">({{ product.size }})</span></h2>
+        <p class="flex flex-col gap-1 text-black/40 text-xs">
           <span v-if="product.viandes && product.viandes.length > 0">Les viandes: {{ product.viandes.join(',  ') }}</span>
           <span v-if="product.sauces && product.sauces.length > 0">Les sauces: {{ product.sauces.join(', ') }}</span>
           <span v-if="product.extras && product.extras.length > 0">Les extras: {{ product.extras.join(', ') }}</span>
